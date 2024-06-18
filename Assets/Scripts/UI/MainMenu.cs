80edb7886bf00a0d2de5fdb2b4643e4dd1080431
왕성민 <wsm6753@gmail.com>
2024-06-18 18:36:12 +0900

    [feat] 실행시 초기 자동선택버튼 기능 및 세부메뉴로 들어갈 시 버튼 비활성화, 활성화 기능 추가

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,24 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject StartMenu;
     [SerializeField] private GameObject LoadMenu;
     [SerializeField] private GameObject OptionMenu;
+    [SerializeField] private List<Button> MainUI_Btn;
+
+    private void Awake()
+    { 
+        FocusBtn(0);
+    }
 
     public void OnClickStartBtn()
-    { 
+    {
         StartMenu.SetActive(true);
+        UnInteractableBtn();
     }
 
     public void OnClickLoadBtn()
     { 
         LoadMenu.SetActive(true);
+        UnInteractableBtn();
     }
 
     public void OnClickOptionBtn()
-    { 
+    {
         OptionMenu.SetActive(true);
+        UnInteractableBtn();
     }
 
     public void OnClickExitBtn()
@@ -28,5 +41,26 @@ public class MainMenu : MonoBehaviour
 #else
         Application.Quit();
 #endif
+    }
+
+    public void FocusBtn(int index)
+    {
+        EventSystem.current.SetSelectedGameObject(MainUI_Btn[index].gameObject);
+    }
+
+    public void InteractableBtn()
+    { 
+        foreach (var btn in MainUI_Btn)
+        {
+            btn.interactable = true;
+        }
+    }
+
+    private void UnInteractableBtn()
+    {
+        foreach (var btn in MainUI_Btn)
+        { 
+            btn.interactable = false;
+        }
     }
 }
