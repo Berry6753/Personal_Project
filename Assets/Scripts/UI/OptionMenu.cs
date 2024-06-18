3f71109f611e8d3968f5a662d66fa9648b9b3e5e
왕성민 <wsm6753@gmail.com>
2024-06-18 17:46:31 +0900

    [feat] OptionMenu에 들어갈 시, 키보드로 선택이 가능하게 생성, 생성해제 시에 버튼 자동 선택 기능 추가

---
+++ Assets/Scripts/UI/OptionMenu.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OptionMenu : MonoBehaviour
+{
+    [SerializeField] private Button Sound_Btn;
+    [SerializeField] private Button Resolution_Btn;
+    [SerializeField] private GameObject SoundOption;
+    [SerializeField] private GameObject ResolutionOption;
+
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(Sound_Btn.gameObject);   
+    }
+
+    private void Update()
+    {
+        Quit();
+    }
+
+    private void Quit()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SoundOption.activeSelf)
+            {
+                SoundOption.SetActive(false);
+                Resolution_Btn.interactable = true;
+                Sound_Btn.interactable = true;
+                EventSystem.current.SetSelectedGameObject(Sound_Btn.gameObject);
+            }
+            else if (ResolutionOption.activeSelf)
+            {
+                ResolutionOption.SetActive(false);
+                Resolution_Btn.interactable = true;
+                Sound_Btn.interactable = true;
+                EventSystem.current.SetSelectedGameObject(Resolution_Btn.gameObject);
+            }
+            else
+                gameObject.SetActive(false);
+        }
+    }
+
+    public void OnClickSoundBtn()
+    { 
+        Sound_Btn.interactable = false;
+        Resolution_Btn.interactable = false;
+        SoundOption.SetActive(true);
+    }
+
+    public void OnClickResolution_Btn()
+    {
+        Sound_Btn.interactable = false;
+        Resolution_Btn.interactable = false;
+        ResolutionOption.SetActive(true);
+    }
+}
