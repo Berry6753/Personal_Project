e9aa443e4f7c4c035cb6a84f6c11b3ff37128b8c
왕성민 <wsm6753@gmail.com>
2024-06-19 16:46:05 +0900

    [test] UI매니저를 통해 MainMenu의 함수를 호출할 때 MainMenu입력을 안 하게 하기위한 테스트
    이렇게 하면 되기는 하나 눈가리고 아웅인 것 같아 다른 방법을 찾아야 할 것 같음

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+
+public enum UIType
+{ 
+    MainUI,
+    InfoPopup
+}
 
 public class UIManager : Singleton<UIManager>
 {
-    [SerializeField] private MainMenu mainMenu;
-    public MainMenu MainMenu { get { return mainMenu; } }
+    //[SerializeField] private MainMenu mainMenu;
+    //public MainMenu MainMenu { get { return mainMenu; } }
 
+    [SerializeField] private MainMenu MainUI;
 
+    public void FocusBtn(int index)
+    { 
+        MainUI.FocusBtn(index);
+    }
+
+    public void InteractableBtn()
+    {
+        MainUI.InteractableBtn();
+    }
 }
