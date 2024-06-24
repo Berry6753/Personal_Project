350fa51fc8e2247de0e830aa84dbed1949d76986
왕성민 <wsm6753@gmail.com>
2024-06-24 18:59:04 +0900

    [Add] 게임매니저 변수에 Ford 추가 및 게임 멈추는 기능 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private PlayerMove player;
-    public PlayerMove Player {  get { return player; } }
+    public PlayerMove Player { get { return player; } }
+    [SerializeField] private FordController ford;
+    public FordController Ford { get { return ford; } }
+
+    private bool isGameOnTime = true;
 
     public void ChangeState()
     {
         player.ChangeStateAfterMove();
+    }
+
+    public void PauseGame()
+    {
+        if (isGameOnTime)
+        {
+            isGameOnTime = false;
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+            isGameOnTime = true;
+        }
     }
 }
