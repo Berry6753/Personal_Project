72704bb6dc63a118d447b5e673cddc3af9cd57eb
왕성민 <wsm6753@gmail.com>
2024-06-27 03:31:25 +0900

    [Add] PlayerStatusViewModelExtension 스크립트 생성

---
+++ Assets/Scripts/Player/PlayerStatusViewModelExtension.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ViewModel.Extensions
+{
+    public static class PlayerStatusViewModelExtension
+    {
+        public static void RefreshViewModel(this PlayerStatusViewModel vm)
+        { 
+            
+        }
+    }
+}
