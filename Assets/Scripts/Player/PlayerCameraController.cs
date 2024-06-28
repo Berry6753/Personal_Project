1a3cbc9c2d55050e44fb92ce7aa9168088ed0650
왕성민 <wsm6753@gmail.com>
2024-06-28 10:20:31 +0900

    [feat] Virtual 카메라에 플레이어를 직접 할당하는 것보다 스크립트로 할당하도록 추가

--- Assets/Scripts/Player/PlayerCameraController.cs
+++ Assets/Scripts/Player/PlayerCameraController.cs
@@ -1,35 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerCameraController : MonoBehaviour
 {
-    [SerializeField] private Transform playerTr;
-    [SerializeField] private Transform cameraTr;
+    private CinemachineVirtualCamera _followCamera;
+    private GameObject _player_CameraRoot;
 
-    private Vector2 mouseDelta = Vector2.zero;
-
-    private void Update()
+    private void Awake()
     {
-        LookAround();
+        _followCamera = GetComponent<CinemachineVirtualCamera>();
+        _player_CameraRoot = GameObject.FindGameObjectWithTag("CameraRoot");
     }
 
-    private void LookAround()
-    {       
-        Vector3 camAngle = cameraTr.rotation.eulerAngles;
-        float x = camAngle.x - mouseDelta.y;
-
-        if (x < 180)
-            x = Mathf.Clamp(x, -1f, 70f);
-        else
-            x = Mathf.Clamp(x, 335f, 361f);
-
-        cameraTr.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
-    }
-
-    public void OnRatation_Camera(InputAction.CallbackContext context)
-    { 
-        mouseDelta = context.ReadValue<Vector2>(); 
+    private void Start()
+    {
+        _followCamera.Follow = _player_CameraRoot.transform;
     }
 }
