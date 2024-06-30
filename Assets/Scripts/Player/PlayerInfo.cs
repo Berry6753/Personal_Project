df8a8978c03fb3bedf54bf0c2310d62012e882a7
왕성민 <wsm6753@gmail.com>
2024-07-01 03:49:50 +0900

    [feat] 기본 체력, 데미지, 경험치 성장치 기능

---
+++ Assets/Scripts/Player/PlayerInfo.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    private float _maxHp;
+    private float _hp;
+    private float _maxExp;
+    private float _minExp;
+    private float _exp;
+    private float _damage;
+
+    private int _level;
+    private int _levelPoint;
+
+    private void Awake()
+    {
+        _hp = _maxExp;
+        _exp = _minExp;
+    }
+
+    private void Update()
+    {
+        if (_exp >= _maxExp)
+        {
+            LevelUp();
+            UpScaleExp();
+        }
+    }
+
+    private void LevelUp()
+    {
+        _level++;
+        _levelPoint += 5;
+        _hp = _maxHp;
+        _exp = _minExp;
+    }
+
+    private void UpScaleExp()
+    {
+        _maxExp *= 1.2f;
+    }
+    private void UpScaleHp()
+    {
+        _maxHp += 20f;
+    }
+    private void UpScaleDmg()
+    {
+        _damage += 20f;
+    }
+}
