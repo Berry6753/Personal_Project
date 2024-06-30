7c22d29b691e9ba314d8f566023bf70bc580dd13
왕성민 <wsm6753@gmail.com>
2024-06-30 18:08:24 +0900

    [feat] EnemyState 기능

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum State
+    { 
+        IDLE,
+        TRACE,
+        ATTACK
+    }
+    public State state = State.IDLE;
+    private StateMachine _stateMachine;
+
+    private void Awake()
     {
-        
+        _stateMachine = gameObject.AddComponent<StateMachine>();
+
+        _stateMachine.AddState(State.IDLE, new IdleState(this));
+        _stateMachine.AddState(State.TRACE, new TraceState(this));
+        _stateMachine.AddState(State.ATTACK, new AttackState(this));
+        _stateMachine.InitState(State.IDLE);
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public class BaseEnemyState : BaseState
     {
-        
+        protected EnemyController enemy;
+        public BaseEnemyState(EnemyController enemy)
+        { 
+            this.enemy = enemy;
+        }
+    }
+    public class IdleState : BaseEnemyState
+    {
+        public IdleState(EnemyController enemy) : base(enemy) { }
+        public override void Enter()
+        {
+            
+        }
+    }
+    public class TraceState : BaseEnemyState
+    { 
+        public TraceState(EnemyController enemy) : base(enemy) { }
+        public override void Enter()
+        {
+
+        }
+    }
+    public class AttackState : BaseEnemyState
+    {
+        public AttackState(EnemyController enemy) : base(enemy) { }
+        public override void Enter()
+        {
+
+        }
     }
 }
