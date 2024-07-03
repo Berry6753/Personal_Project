8bcd10cdf1ed975caa84962451543b56d0a01a0a
왕성민 <wsm6753@gmail.com>
2024-07-03 15:39:06 +0900

    [Add] PlayerUIManager 생성

---
+++ Assets/Scripts/Manager/PlayUIManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayUIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject saveUI;
+    [SerializeField] private GameObject pauseUI;
+
+
+}
