80a4d994e966f1595635afe48dbfa80d593a44d1
왕성민 <wsm6753@gmail.com>
2024-07-05 18:42:59 +0900

    [feat] 위클라인 총알이 생성되어 움직이는 기능

---
+++ Assets/Scripts/Enemy/WicklineBulletMove.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class WicklineBulletMove : MonoBehaviour
+{
+    private Vector3 _direction;
+
+    private float _bulletSpeed = 30.0f;
+
+    private bool _isDestroyed;
+    
+    private IObjectPool<WicklineBulletMove> _pool;
+
+    private void OnEnable()
+    {
+        _isDestroyed = false;
+        CancelInvoke("DestroyBullet");
+    }
+    private void Update()
+    {
+        transform.Translate(_direction * _bulletSpeed * Time.deltaTime);
+    }
+
+    public void Shoot()
+    {
+        _direction = Vector3.forward;
+        Invoke("DestroyBullet", 5f);
+    }
+
+    public void SetManagedPool(IObjectPool<WicklineBulletMove> pool)
+    {
+        _pool = pool;
+    }
+
+    public void DestroyBullet()
+    {
+        if (_isDestroyed) return;
+        _isDestroyed = true;
+        _pool.Release(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{other}");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //todo ÇÇÇØ        
+        }
+        DestroyBullet();
+    }
+}
