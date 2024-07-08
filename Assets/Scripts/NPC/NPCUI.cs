6fc75674e76583274a98adaba7bd99ee4fe2d35a
왕성민 <wsm6753@gmail.com>
2024-07-08 20:49:36 +0900

    [feat] 대화 기능 추가

--- Assets/Scripts/NPC/NPCUI.cs
+++ Assets/Scripts/NPC/NPCUI.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 public class NPCUI : MonoBehaviour
 {
     [SerializeField] private GameObject interactionUI;
+    [SerializeField] private GameObject canvus;
+    [SerializeField] private GameObject DiaLog;
+
+    private bool isNear = false;
+    private bool isDialog;
 
     private void OnEnable()
     {
        interactionUI.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (isNear)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                isDialog = true;
+            }
+        }
+
+        if (!isDialog) return;
+
+        canvus.SetActive(true);
+        DiaLog.SetActive(true);
+
+        if (!DiaLog.activeSelf)
+        {
+            isDialog = false;
+        }
+    }
+
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         { 
             interactionUI.SetActive(true);
+            isNear = true;
         }
     }
     private void OnTriggerExit(Collider other)
@@ -23,6 +52,7 @@ public class NPCUI : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             interactionUI.SetActive(false);
+            isNear = false;
         }
     }
 }
