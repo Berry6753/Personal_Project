f53940ea4e10fded4374334b76633e4730c937eb
왕성민 <wsm6753@gmail.com>
2024-07-08 20:47:05 +0900

    [Add] 마을 안내 UI 활성화 기능

--- Assets/Scripts/Manager/PlayUIManager.cs
+++ Assets/Scripts/Manager/PlayUIManager.cs
@@ -1,10 +1,33 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayUIManager : MonoBehaviour
+public class PlayUIManager : Singleton<PlayUIManager>
 {
     [SerializeField] private GameObject saveUI;
     [SerializeField] private GameObject pauseUI;
+    [SerializeField] private GameObject townNoticeUI;
+    [SerializeField] private TMP_Text Text_TownNotice;
+    Coroutine co;
 
+    public void TownNotice(string text)
+    {
+        if (co == null)
+            co = StartCoroutine(CoTownNotice(text));
+        else
+        { 
+            StopCoroutine(co);
+            co = StartCoroutine(CoTownNotice(text));
+    }
+    }
 
+    private IEnumerator CoTownNotice(string text)
+    {
+        townNoticeUI.SetActive(false);
+        townNoticeUI.SetActive(true);
+        Text_TownNotice.text = text;
+        yield return new WaitForSeconds(5.0f);
+        townNoticeUI.SetActive(false);
+    }
 }
