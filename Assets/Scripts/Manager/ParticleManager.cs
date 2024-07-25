a2088d95804a9532e4d7209d8ce01f9ab48f6406
왕성민 <wsm6753@gmail.com>
2024-07-25 11:07:09 +0900

    [fix] 벽 파티클 추가 및 인스펙터창에 노출될 헤더 추가

--- Assets/Scripts/Manager/ParticleManager.cs
+++ Assets/Scripts/Manager/ParticleManager.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class ParticleManager : Singleton<ParticleManager>
 {
+    [Header("알파, 오메가 공격 파티클")]
     [SerializeField] private ParticleSystem aoParticle;
     public ParticleSystem AOParticle { get { return aoParticle; } }
+
+    [Header("위클라인 공격 파티클")]
     [SerializeField] private ParticleSystem wicklineParticle;
     public ParticleSystem WicklineParticle { get {  return wicklineParticle; } }
+
+    [Header("통행금지 벽 파티클")]
+    [SerializeField] private ParticleSystem wallParticle;
+    public ParticleSystem WallParticle { get { return wallParticle; } }
+
+    public void OnPlayWallParticle(Vector3 pos)
+    { 
+        wallParticle.transform.position = pos;
+        wallParticle.Play();
+    }
+    public void OnStopWallParticle()
+    { 
+        wallParticle.Stop();
+    }
 }
